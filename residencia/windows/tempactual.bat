8bb0db0668285e40c8444b9038f9450feb79baf4
levitco <47256157+levitco@users.noreply.github.com>
2019-03-07 09:17:17 -0600

    Add files via upload

--- residencia/windows/tempactual.bat
+++ residencia/windows/tempactual.bat
@@ -8,7 +8,9 @@ exit
 echo "Copiando a C:\registro_de_temperaturas.txt"
 scp levitico@192.168.: /home/levitico/registro_de_temperaturas.txt C:\registro_de_temperaturas.txt
 echo "Copiado con exito"
-echo "Cerrando"
-timeout 5
+echo "abriendo archivo"
+cd C:
+notepad registro_de_temperaturas.txt
+exit
 
 
