57c6352d7171f306343f6012bf65ccd2bd477cf8
levitco <47256157+levitco@users.noreply.github.com>
2019-03-07 09:09:36 -0600

    Add files via upload

---
+++ residencia/windows/tempactual.bat
@@ -0,0 +1,14 @@
+echo off
+ssh levitico@192.168.
+echo "Conexion realizada con exito"
+echo "Registrando temperatura"
+tempa
+echo "Registro exitoso"
+exit
+echo "Copiando a C:\registro_de_temperaturas.txt"
+scp levitico@192.168.: /home/levitico/registro_de_temperaturas.txt C:\registro_de_temperaturas.txt
+echo "Copiado con exito"
+echo "Cerrando"
+timeout 5
+
+
